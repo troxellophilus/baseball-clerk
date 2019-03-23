9bf0e6a0898ece06e9ed26fa7528e95401af4129
Drew Troxell <code@trox.space>
2019-03-23 11:42:31 -0700

    Bump to release 1.0.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     author='Drew Troxell',  # Optional
     author_email='code@trox.space',
     classifiers=[  # Optional
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.7',
